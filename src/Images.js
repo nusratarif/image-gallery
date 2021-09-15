6769f51bedd0df8632f092cdc90e4b12d713a0de
nusratarif <nusratarif02@gmail.com>
2021-09-15 23:11:44 +0500

    added package-lock.json and src/Images.js and src/Search.js

---
+++ src/Images.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Images extends React.Component {
+    render() {
+        const { image } = this.props;
+        console.log(image)
+        return (
+           
+                <div className="col-md-3">
+                    <img src={image.src.large}
+                        alt="not found"
+                        className="img-fluid"
+                    />
+                </div>
+           
+        )
+    }
+
+}
+export default Images;
+
+
